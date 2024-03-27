d428240e24262596672ac6ddc7bb7f84fc293e53
mariusjacob-batstateu <21-07121@g.batstate-u.edu.ph>
2024-03-27 09:10:54 +0800

    face recog algo
    Face recognition is done, GUI under development

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -1,2 +1,39 @@
 import cv2
+import os
 import face_recognition
+import pickle
+
+# importing face student images into a list
+folderStudentPath= "students"
+StudentPathList = os.listdir(folderStudentPath)
+imgStudentList= []
+studentIDs=[]
+
+for path in StudentPathList:
+    imgStudentList.append(cv2.imread(os.path.join(folderStudentPath, path)))
+    studentIDs.append(os.path.splitext(path)[0])
+    
+
+
+# loop thru image and encode every single
+def findEncodings(imagesList):
+    encodeList=[]
+    for img in imagesList:
+        img= cv2.cvtColor(img,cv2.COLOR_BGR2RGB) #opencv is bgr but face-recognition is rgb
+        encode=face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+        
+        
+    return encodeList
+        
+
+print("start")
+encodeListKnown= findEncodings(imgStudentList)
+encodeListKnownWithIDs=[studentIDs, encodeListKnown]
+
+
+# putting the face encodings with ID in pickle file
+file=open("EncodeFile.p", 'wb')
+pickle.dump(encodeListKnownWithIDs, file)
+file.close()
+print("complete")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import os
+import pickle
+import face_recognition
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -13,13 +15,34 @@ imgModeList= []
 for path in modePathList:
     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
 
+# load encodefile
+file= open("EncodeFile.p", 'rb')
+encodeListKnownWithIDs= pickle.load(file)
+file.close()
+studentIDs, encodeListKnown= encodeListKnownWithIDs
 
 
+print(studentIDs)
 while True:
     success, img= cap.read()
-
+    
+    imgSmall= cv2.resize(img, (0,0), None, 0.25, 0.25) #make the video smaller, for small computational power
+    imgSmall= cv2.cvtColor(imgSmall,cv2.COLOR_BGR2RGB)
+    
+    faceCurrFrame= face_recognition.face_locations(imgSmall) #looks for the face
+    encodeCurrFrame= face_recognition.face_encodings(imgSmall, faceCurrFrame) #matches the current face with the encodings, not the whole image
+    
+    
     imgBackground[162:162+480, 55:55+640]= img
     imgBackground[44:44+633, 808:808+414]=imgModeList[0]
+    
+    for encodeFace, faceLoc in zip(encodeCurrFrame, faceCurrFrame):
+        matches=face_recognition.compare_faces(encodeListKnown, encodeFace)
+        matches=face_recognition.compare_faces(encodeListKnown, encodeFace) #lower distance, better match
+        faceDistance=face_recognition.face_distance(encodeListKnown, encodeFace)
+        print("Matches: ", matches)
+        print("Face distance: ", faceDistance)
+    
     cv2.imshow("Face attendance", imgBackground) 
     cv2.waitKey(1)
     
